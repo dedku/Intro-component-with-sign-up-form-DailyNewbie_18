32be05688f16ce904157fb79b9f6e77b3758a305
Patryk Szabat <szabat.patryk@gmail.com>
2022-12-28 18:19:27 +0100

    css + ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-export const test = []
+export const firstName = document.getElementById('first-name') as HTMLInputElement
+export const lastName = document.getElementById('last-name') as HTMLInputElement
+export const email = document.getElementById('email') as HTMLInputElement
+export const button = document.getElementById('btn') as HTMLButtonElement
+const error = document.getElementById('error') as HTMLDivElement
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g as RegExp
+    const fullNameCheck = firstName.value.length > 3 && lastName.value.length > 3 as boolean
+    const inputValue = email.value.length > 0 as boolean
+    const verifieEmail = email.value.match(regex) as RegExpMatchArray | null
+    if (!fullNameCheck) {
+        error.innerHTML = `Name fields are empty or less then 4 letters`
+    }
+    if (!inputValue) {
+        error.innerHTML = `Email field is empty`
+    }
+    if (inputValue && !verifieEmail) {
+        error.innerHTML = `Please provide a valid email address`
+    }
+    if (verifieEmail && fullNameCheck) {
+        error.innerHTML = ``
+    }
+})
